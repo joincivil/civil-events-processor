4dea54d8611e0b7a28de313718c4fb8f265cfa8d
inna shteinbuk <ibs26@cornell.edu>
2018-12-03 14:20:53 -0500

    commenting out this test bc link is broken

--- pkg/scraper/metadata_test.go
+++ pkg/scraper/metadata_test.go
@@ -1,36 +1,36 @@
 package scraper_test
 
-import (
-	"strings"
-	"testing"
+// import (
+// 	"strings"
+// 	"testing"
 
-	"github.com/joincivil/civil-events-processor/pkg/scraper"
-)
+// 	"github.com/joincivil/civil-events-processor/pkg/scraper"
+// )
 
-const (
-	// This may go away at some point, so see if this fails
-	civilTestAddress = "https://civil-develop.go-vip.co/crawler-pod/wp-json/civil-newsroom-protocol/v1/revisions/11"
-)
+// const (
+// 	// This may go away at some point, so see if this fails
+// 	civilTestAddress = "https://civil-develop.go-vip.co/crawler-pod/wp-json/civil-newsroom-protocol/v1/revisions/11"
+// )
 
-func TestCivilMetadataScraper(t *testing.T) {
-	_scraper := &scraper.CivilMetadataScraper{}
-	metadata, err := _scraper.ScrapeCivilMetadata(civilTestAddress)
+// func TestCivilMetadataScraper(t *testing.T) {
+// 	_scraper := &scraper.CivilMetadataScraper{}
+// 	metadata, err := _scraper.ScrapeCivilMetadata(civilTestAddress)
 
-	if err != nil {
-		if strings.Contains(err.Error(), "Error retrieving metadata") {
-			t.Logf("Looks like issue retrieving remote data: err: %v", err)
-		} else {
-			t.Errorf("Should not have been an error scraping metadata: err: %v", err)
-		}
-	}
+// 	if err != nil {
+// 		if strings.Contains(err.Error(), "Error retrieving metadata") {
+// 			t.Logf("Looks like issue retrieving remote data: err: %v", err)
+// 		} else {
+// 			t.Errorf("Should not have been an error scraping metadata: err: %v", err)
+// 		}
+// 	}
 
-	if metadata.Title() == "" {
-		t.Errorf("Should not have a empty title")
-	}
-	if metadata.Slug() == "" {
-		t.Errorf("Should not have a empty slug")
-	}
-	if metadata.CanonicalURL() == "" {
-		t.Errorf("Should not have a empty canonical URL")
-	}
-}
+// 	if metadata.Title() == "" {
+// 		t.Errorf("Should not have a empty title")
+// 	}
+// 	if metadata.Slug() == "" {
+// 		t.Errorf("Should not have a empty slug")
+// 	}
+// 	if metadata.CanonicalURL() == "" {
+// 		t.Errorf("Should not have a empty canonical URL")
+// 	}
+// }
